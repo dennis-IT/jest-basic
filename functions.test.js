6c1b45d9888efb9f68783cf7b6ad02cce6705737
dennis-IT <dennis.huy.ta@gmail.com>
2021-01-19 15:11:13 -0500

    Updated test file

--- functions.test.js
+++ functions.test.js
@@ -9,3 +9,19 @@ test('Adds 2 + 2 to NOT equal 5', () => {
 });
 
 //!NOTE: 
+/*
+Check for truthy and falsy value
+toBeNull matches only null value
+toBeUndefined matches only undefined value
+toBeDefined is the opposite of toBeUndefined
+toBeTruthy matches anything that an if statement treats as true
+toBeFalsy matches anything that an if statement treats as false
+*/
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+test('Should be undefined', () => {
+    expect(functions.isUndefined()).toBeUndefined();
+});
